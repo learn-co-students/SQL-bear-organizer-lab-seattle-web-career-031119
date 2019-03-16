bc64c729404280c7f48304de1a81dabc42cdab88
Rylan Bauermeister <apple@MacBook-Pro.local>
2019-03-15 17:49:41 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "Male", "Brown", "Docile", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 17, "Male", "Black", "Cray", FALSE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "Female", "Grey", "Gentle", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 29, "Male", "Brown", "Aggressive", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 13, "Female", "Brown", "Curious", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 12, "Male", "Green", "Hostile", FALSE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 6, "Female", "Brown", "Insane", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 300, "Male", "Brown", "Murderer", TRUE);
